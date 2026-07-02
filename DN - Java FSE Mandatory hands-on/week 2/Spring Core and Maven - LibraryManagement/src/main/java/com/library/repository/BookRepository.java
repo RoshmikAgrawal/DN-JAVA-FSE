bce0fe22f4883af8dbe5eb76977fc05ffd504c0e
RoshmikAgrawal <roshmikagrawal@gmail.com>
2026-07-03 01:08:24 +0530

    Added Spring Core and Maven exercises

---
+++ DN - Java FSE Mandatory hands-on/week 2/Spring Core and Maven - LibraryManagement/src/main/java/com/library/repository/BookRepository.java
@@ -0,0 +1,7 @@
+package com.library.repository;
+
+public class BookRepository {
+    public void getBookDetails() {
+        System.out.println("Fetching collection metadata entries from database repository logs...");
+    }
+}
